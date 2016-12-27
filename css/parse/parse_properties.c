8ebb329dd5e7ba41fec37b07bf5b49583b9ce75a
Drew DeVault <sir@cmpwn.com>
2016-12-26 23:22:39 -0500

    Implement initial support for quoted CSS values

--- css/parse/parse_properties.c
+++ css/parse/parse_properties.c
@@ -12,6 +12,7 @@
 struct properties_state {
 	style_rule_t *style_rule;
 	str_t *key, *value;
+	uint32_t quotes;
 };
 
 static void properties_state_free(void *_state) {
@@ -26,7 +27,7 @@ static void properties_state_free(void *_state) {
 struct subparser_state *push_properties_parser(struct parser_state *state,
 		style_rule_t *style_rule) {
 	struct subparser_state *subparser = push_parser(state,
-			parse_properties);
+		parse_properties);
 	subparser->destructor = properties_state_free;
 	struct properties_state *pstate = calloc(sizeof(struct properties_state), 1);
 	pstate->style_rule = style_rule;
@@ -39,40 +40,55 @@ void parse_properties(stylesheet_t *stylesheet,
 		struct parser_state *pstate, uint32_t ch) {
 	struct subparser_state *subparser = list_peek(pstate->parsers);
 	struct properties_state *state = subparser->state;
-	switch (ch) {
-	case ':':
-		if (state->value) {
-			// TODO: error
+	if (state->quotes) {
+		// TODO: Handle escape sequences
+		if (ch == state->quotes) {
+			subparser->flags &= ~FLAG_WHITESPACE;
+			state->quotes = '\0';
 		} else {
-			state->value = str_create();
-		}
-		break;
-	case ';':
-		if (!state->value || !state->key) {
-			// TODO: error
-		} else {
-			hashtable_set(state->style_rule->properties,
-					state->key->str, state->value->str);
-			free(state->key);
-			free(state->value);
-			state->key = str_create();
-			state->value = NULL;
-		}
-		break;
-	case '}':
-		list_add(stylesheet->rules, state->style_rule);
-		list_pop(pstate->parsers);
-		break;
-	case '\'': // Fallthrough
-	case '"':
-		// TODO
-		break;
-	default:
-		if (state->value) {
 			str_append_ch(state->value, ch);
-		} else {
-			str_append_ch(state->key, ch);
 		}
-		break;
+	} else {
+		switch (ch) {
+		case ':':
+			if (state->value) {
+				// TODO: error
+			} else {
+				state->value = str_create();
+			}
+			break;
+		case ';':
+			if (!state->value || !state->key) {
+				// TODO: error
+			} else {
+				hashtable_set(state->style_rule->properties,
+						state->key->str, state->value->str);
+				free(state->key);
+				free(state->value);
+				state->key = str_create();
+				state->value = NULL;
+			}
+			break;
+		case '}':
+			list_add(stylesheet->rules, state->style_rule);
+			list_pop(pstate->parsers);
+			break;
+		case '\'': // Fallthrough
+		case '"':
+			if (state->value) {
+				subparser->flags |= FLAG_WHITESPACE;
+				state->quotes = ch;
+			} else {
+				// TODO: error
+			}
+			break;
+		default:
+			if (state->value) {
+				str_append_ch(state->value, ch);
+			} else {
+				str_append_ch(state->key, ch);
+			}
+			break;
+		}
 	}
 }
--- test/css/css_parse.c
+++ test/css/css_parse.c
@@ -102,6 +102,26 @@ static int test_multi_properties() {
 	return 0;
 }
 
+static int test_quoted_properties() {
+	stylesheet_t *ss = css_parse("foo { \n"
+			"\ttext: \"hello: {world};!\";\n"
+			"\theader: 'hello: {world};!';\n"
+			"}");
+	assert(ss->rules->length == 1);
+
+	style_rule_t *rule = ss->rules->items[0];
+	assert(rule);
+
+	char *value = hashtable_get(rule->properties, "text");
+	assert(value && strcmp(value, "hello: {world};!") == 0);
+
+	value = hashtable_get(rule->properties, "header");
+	assert(value && strcmp(value, "hello: {world};!") == 0);
+
+	stylesheet_free(ss);
+	return 0;
+}
+
 int test_main() {
 	return test_comments()
 		|| test_selector()
@@ -109,5 +129,6 @@ int test_main() {
 		|| test_multiple_selectors()
 		|| test_properties()
 		|| test_multi_properties()
+		|| test_quoted_properties()
 	;
 }
