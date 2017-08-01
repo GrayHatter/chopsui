6fc3f1c3dc87c8e1d1e1ef55098b955645a73cdb
Drew DeVault <sir@cmpwn.com>
2017-07-31 20:49:24 -0400

    Use selector_t in tree.h

--- include/tree.h
+++ include/tree.h
@@ -9,32 +9,32 @@
 /**
  * Finds the first ancestor of node that matches the provided CSS selector.
  */
-sui_node_t node_find_ancestor(sui_node_t *node, const char *selector);
+sui_node_t node_find_ancestor(sui_node_t *node, selector_t *selector);
 /**
  * Creates and returns a list_t of references to ancestors of node that match
  * the provided CSS selector.
  */
-list_t *node_find_ancestors(sui_node_t *node, const char *selector);
+list_t *node_find_ancestors(sui_node_t *node, selector_t *selector);
 /**
  * Invokes the provided iter function with each anscestor of the node that
  * matches the provided CSS selector.
  */
-void node_iter_ancestors(sui_node_t *node, const char *selector, void (*iter)(sui_node_t *node));
+void node_iter_ancestors(sui_node_t *node, selector_t *selector, void (*iter)(sui_node_t *node));
 
 /**
  * Finds the first descendant of node that matches the provided CSS selector.
  */
-sui_node_t node_find_descendant(sui_node_t *node, const char *selector);
+sui_node_t node_find_descendant(sui_node_t *node, selector_t *selector);
 /**
  * Creates and returns a list_t of references to descendants of node that
  * match the provided CSS selector.
  */
-list_t *node_find_descendants(sui_node_t *node, const char *selector);
+list_t *node_find_descendants(sui_node_t *node, selector_t *selector);
 /**
  * Invokes the provided iter function with each descendant of node that matches the
  * provided CSS selector.
  */
-void node_iter_descendants(sui_node_t *node, const char *selector, void (*iter)(sui_node_t *node));
+void node_iter_descendants(sui_node_t *node, selector_t *selector, void (*iter)(sui_node_t *node));
 
 /**
  * Adds the node to the end of the parent's children. Returns the number of
